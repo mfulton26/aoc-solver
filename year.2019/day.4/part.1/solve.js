469431c2122b75246a85c6a82be7e7041a7a3388
Mark Fulton <mark.fulton@asurion.com>
2019-12-07 14:28:02 -0600

    Year 2019 Day 1 Part 1

---
+++ year.2019/day.4/part.1/solve.js
@@ -0,0 +1,20 @@
+const adjacentDigitsRegExp = /(\d)\1/;
+
+export default function(input) {
+  const [start, end] = input.split("-").map(Number);
+  let count = 0;
+  for (let n = start; n <= end; n++) {
+    const s = String(n);
+    if (
+      s.length === 6 &&
+      adjacentDigitsRegExp.test(s) &&
+      s ===
+        Array.from(s)
+          .sort()
+          .join("")
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
